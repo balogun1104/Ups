61fb73f794fa7c5b76c1ae28e8a9d952c90c8c46
Balogun Razak <razak2free@gmail.com>
2022-11-25 17:14:33 +0100

    summary

---
+++ Graphql/queries.ts
@@ -0,0 +1,44 @@
+import { gql, useQuery } from '@apollo/client';
+
+export const GET_CUSTOMERS = gql`
+  query GetCustomers   {
+    getCustomers {
+      name
+      value {
+        email
+        name
+      }
+    }
+  }
+`;
+
+export const GET_ORDERS = gql`
+  query GetOrders   {
+    getOrders {
+      name
+      value {
+        Address
+        City
+        Lat
+        Lng
+        createdAt
+        carrier
+        trackingId
+        shippingCost
+        trackingItems {
+          customer {
+            email
+            name
+          }
+          customer_id
+          items {
+            item_id
+            name
+            price
+            quantity
+          }
+        }
+      }
+    }
+  }
+`;
