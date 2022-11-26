0e793b171fb1dd1a867f02863bdff6642847c7a4
Balogun Razak <razak2free@gmail.com>
2022-11-26 06:49:12 +0100

    devFestDay!!!

---
+++ hooks/useOrders.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import { useQuery } from '@apollo/client'
+import { GET_ORDERS } from '../graphql/queries'
+
+const useOrders = () => {
+    const { data, loading, error } = useQuery(GET_ORDERS);
+    const [orders, setOrder] = useState<Order[]>([]);
+
+    useEffect(() => {
+        if (!data) return;
+
+        const orders: Order[] = data.getOrders.map(({ value }: OrderResponse) => ({
+            Address: value.Address,
+            City: value.City,
+            Lat: value.Lat,
+            Lng: value.Lng,
+            carrier: value.carrier,
+            createdAt: value.createdAt,
+            shippingCost: value.shippingCost,
+            trackingId: value.trackingId,
+            trackingItems: value.trackingItems,
+        }));
+        setOrder(orders)
+    },[data])
+
+  return {data, loading, orders}
+}
+
+export default useOrders
--- typings.d.ts
+++ typings.d.ts
@@ -1,9 +1,45 @@
- type Customer = {
-    email: string;
-    name: string;
- }
+type Customer = {
+  email: string;
+  name: string;
+};
 
- type CustomerList = {
-    name: ID;
-    value: Customer;
- }
+type CustomerList = {
+  name: ID;
+  value: Customer;
+};
+
+type TrackingItem = {
+  customer_id: ID;
+  customer: Customer;
+  items: Item[];
+};
+
+type Item = {
+  item_id: ID;
+  name: string;
+  price: number;
+  quantity: number;
+};
+
+type OrderResponse = {
+  value: Order;
+};
+
+type CustomerResponse = {
+  name: ID;
+  value: Customer;
+};
+
+type Order = {
+  Address: String;
+  City: String;
+  Lat: number;
+  Lng: number;
+  carrier: String;
+  createdAt: Date;
+  shippingCost: Int;
+  shippingCost: number;
+  trackingId: String;
+  trackingItems: TrackingItems;
+  
+};
